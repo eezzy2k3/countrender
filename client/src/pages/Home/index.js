f162b34433bb95fbb4052c755cc78c00153d9e86
Octave Music <mailoctavemusic@gmail.com>
2022-11-25 04:37:22 +0100

    style:styled home page and countdown route

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,4 +1,14 @@
+import "./Home.css";
 const Home = () => {
-  return "Home";
+  return (
+    <div className="home flex">
+      <h1>
+        {" "}
+        Welcom<span>e to Coun</span>t Down
+      </h1>
+      <h3> Where We help you countdown to special Events</h3>
+      <button className="button add__button">Add Event +</button>
+    </div>
+  );
 };
 export default Home;
